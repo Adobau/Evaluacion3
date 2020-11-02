38faf9c194098a365afd58affa44e090e1e1f7b2
Adonis Bautista <adonisbautista39@gmail.com>
2020-11-01 21:52:22 -0600

    Evaluacion 3

---
+++ app/src/main/java/com/example/app_crud_mysql/Setting_VAR.java
@@ -0,0 +1,23 @@
+package com.example.app_crud_mysql;
+
+
+public class Setting_VAR {
+    public static final String TAG = "InicioActivity";
+
+
+    public static final String mensajex="";
+
+
+
+    public static final String URL_guardar_categoria = "http://192.168.1.14/service2020/guardar_categorias.php";
+    public static final String URL_consultaAllCategorias = "http://192.168.1.14/service2020/buscar_Categorias.php";
+    public static final String URL_registrar_productos = "http://192.168.1.14/service2020/guardar_producto.php";
+    public static final String URL_consultaCategoria_id = "http://192.168.1.14/service2020/buscar_categoria_id.php";
+    public static final String URL_update_categoria = "http://192.168.1.14/service2020/actualizar_categoria.php";
+    public static final String URL_delete_categoria = "http://192.168.1.14/service2020/eliminar_categoria.php";
+    public static final String URL_Login = "http://192.168.1.14/service2020/validar_usuarios.php";
+
+
+
+
+}
